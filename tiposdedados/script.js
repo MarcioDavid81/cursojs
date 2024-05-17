8460b26b6a23f76cd3f9b20451152937b9ed413f
marciodavid <marciodavid81@gmail.com>
2024-05-16 22:56:03 -0300

    feat: add new type

--- tiposdedados/script.js
+++ tiposdedados/script.js
@@ -1,11 +1,19 @@
 var nome = "Marcio";
+var idade = 30;
+ console.log(typeof idade);
+
+ // Declare uma variável contendo uma string
+var time = "Grêmio";
+// Declare uma variável contendo um número dentro de uma string
+var ultimoTitulo = "2018";
+// Declare uma variável com a sua idade
 var idade = 42;
-var comidaFavorita;
-    comidaFavorita = "Sushi";
-var numeroFavorito;
-var corFavorita;
-var cidade;
-var estado;
-var pais;
-
-
+// Declare duas variáveis, uma com seu nome
+var nome = "Marcio";
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = "David";
+var nomeCompleto = `${nome} ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+console.log(nomeCompleto);
